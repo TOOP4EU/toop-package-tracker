0a4eb594f0aae6099c0d92045fa77ea9d7c5d9a9
Anton Wiklund <ixuz07@gmail.com>
2018-02-21 17:34:45 +0100

    All cards have their own sass-files. Cleaned up the project a bit.

--- src/main/java/eu/toop/tooppackagetracker/PackageTrackerUI.java
+++ src/main/java/eu/toop/tooppackagetracker/PackageTrackerUI.java
@@ -2,41 +2,17 @@ package eu.toop.tooppackagetracker;
 
 import com.vaadin.annotations.PreserveOnRefresh;
 import com.vaadin.annotations.Push;
-import com.vaadin.annotations.StyleSheet;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.spring.annotation.EnableVaadin;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.spring.server.SpringVaadinServlet;
 import com.vaadin.ui.*;
 import eu.toop.tooppackagetracker.parallax.ParallaxLayout;
-import eu.toop.tooppackagetracker.parallax.Slice;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.ContextLoaderListener;
-
-import javax.servlet.annotation.WebListener;
-import javax.servlet.annotation.WebServlet;
-
-@StyleSheet({
-                /*
-                 * JQuery UI
-                 */
-//        "vaadin://jquery/jquery-ui-1.9.2.custom/css/ui-darkness/jquery-ui-1.9.2.custom.min.css",
-})
 
 @com.vaadin.annotations.JavaScript({
-                /*
-                 * JQuery
-                 */
         "vaadin://jquery/jquery-3.3.1.js",
         "vaadin://js/package-tracker.js",
-
-                /*
-                 * JQuery UI
-                 */
-//        "vaadin://jquery/jquery-ui-1.9.2.custom/js/jquery-ui-1.9.2.custom.min.js",
 })
 
 @Theme("PackageTrackerUITheme")
@@ -46,18 +22,15 @@ import javax.servlet.annotation.WebServlet;
 @PreserveOnRefresh
 public class PackageTrackerUI extends UI implements Receiver.Listener {
 
-  private static String BOOT_TOPIC = "boot.t";
-
   final VerticalLayout mainLayout = new VerticalLayout();
   final ParallaxLayout parallaxLayout = new ParallaxLayout();
-  final VerticalLayout controlLayout = new VerticalLayout();
   JavaScript javaScript;
 
   @Autowired
-  private Sender sender;
+  private Sender kafkaProducer;
 
   @Autowired
-  private Receiver receiver;
+  private Receiver kafkaConsumer;
 
   @Override
   public void receive(ConsumerRecord<?, ?> consumerRecord) {
@@ -72,20 +45,6 @@ public class PackageTrackerUI extends UI implements Receiver.Listener {
     });
   }
 
-  @WebServlet(value = "/*", asyncSupported = true)
-  public static class Servlet extends SpringVaadinServlet {
-  }
-
-  @WebListener
-  public static class MyContextLoaderListener extends ContextLoaderListener {
-  }
-
-  @Configuration
-  @EnableVaadin
-  public static class MyConfiguration {
-
-  }
-
   @Override
   protected void init(VaadinRequest request) {
     mainLayout.setHeight("100%");
@@ -93,18 +52,9 @@ public class PackageTrackerUI extends UI implements Receiver.Listener {
     mainLayout.setStyleName("mainLayout");
     setContent(mainLayout);
 
-    receiver.addListener(this);
+    kafkaConsumer.addListener(this);
     javaScript = JavaScript.getCurrent();
 
-    //mainLayout.addComponent(controlLayout);
     mainLayout.addComponent(parallaxLayout);
-
-    Button mockButton = new Button("Mock input");
-    mockButton.addClickListener(new Button.ClickListener() {
-      public void buttonClick(Button.ClickEvent event) {
-        sender.send("toop", "A toop message");
-      }
-    });
-    controlLayout.addComponent(mockButton);
   }
 }
