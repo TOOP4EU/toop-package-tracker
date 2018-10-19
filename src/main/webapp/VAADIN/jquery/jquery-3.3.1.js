84acb8acf8800222e64a23f8a86b6d8438d7b140
Philip Helger <philip@helger.com>
2018-10-19 18:30:48 +0200

    Added license headers

--- src/main/webapp/VAADIN/jquery/jquery-3.3.1.js
+++ src/main/webapp/VAADIN/jquery/jquery-3.3.1.js
@@ -1,15 +1,17 @@
-/*!
- * jQuery JavaScript Library v3.3.1
- * https://jquery.com/
+/*
+ * Copyright (C) 2018 toop.eu
  *
- * Includes Sizzle.js
- * https://sizzlejs.com/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Copyright JS Foundation and other contributors
- * Released under the MIT license
- * https://jquery.org/license
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- * Date: 2018-01-20T17:24Z
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 ( function( global, factory ) {
 
